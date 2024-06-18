a2b43ccb637e6dcc09e53f58080c144d4f99d8f6
Kafara-droid <fajar.nugraha_ti20@nusaputra.ac.id>
2024-06-18 22:01:44 +0700

    update cloudbuild

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -11,7 +11,21 @@ const {
   sendPasswordResetEmail,
 } = require("firebase/auth");
 
-const serviceAccount = require("../firebaseService.json");
+const privateKey = process.env.private_key.replace(/\\n/g, '\n');
+
+const serviceAccount = {
+  type: process.env.type,
+  project_id: process.env.project_id,
+  private_key_id: process.env.private_key_id,
+  private_key: privateKey,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain
+};
 
 try {
   admin.initializeApp({
